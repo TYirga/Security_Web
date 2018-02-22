203d5e19226b5925b45bd2ecbbed77b237304b14
Tizazu <35386625+TYirga@users.noreply.github.com>
2018-02-22 09:15:01 -0500

    edited

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -1,48 +1,45 @@
 package com.example.demo;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure (HttpSecurity http) throws Exception {
-        http
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return new SSUDS(userRepository);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/add","/addexperiance/**").hasAuthority("USER")
+                .antMatchers("/","/register").permitAll()
+               // .antMatchers("/addreference").hasAuthority("APPLICANT")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login").permitAll()
                 .and()
-                .httpBasic();
 
-        http.headers().frameOptions().disable();
-        http.csrf().disable();
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll().logoutSuccessUrl("/");
     }
-
-
-
-
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("password").authorities("APPLICANT");
 
-
-        auth.inMemoryAuthentication().
-                withUser("user").password("password").authorities("USER")
-                .and()
-                .withUser("mgr").password("employer").authorities("ADMIN");
-
-
+        //Gets information from the database. See the code comments in the SSUDS class for user details. Haha.
+        auth.userDetailsService(userDetailsServiceBean());
     }
 }
-
